b6d39201c73a8974022e21f6b9647ac108548082
zPhumzani <zphumzani@gmail.com>
2016-02-22 00:32:21 +0200

    Initial commit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,22 +21,6 @@ class Admin::UsersController < Admin::ApplicationController
   def edit
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
